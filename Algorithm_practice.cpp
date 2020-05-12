7523775ae6239ba67248f93d9bd8cf101671a9ce
jigeonho <jce0831@gmail.com>
2020-05-12 14:20:32 +0900

    settting

--- Algorithm_practice.cpp
+++ Algorithm_practice.cpp
@@ -2,24 +2,37 @@
 //
 
 #include <iostream>
-
+int getvalue(int);
+int input[100] = { 0 };
+int dp[100] = { 0 };
 int main()
 {
-    std::cout << "Hello World!\n";
 	int n = 0;
-	n = scanf("%d", &n);
-	printf("%d", n);
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	scanf("%d", &n);
+	int m = 0;
 
+	for (int i = 0; i < n; i++) {
+
+		scanf("%d", &m);
+		printf("%d\n", getvalue(m));
+		
+	}
 	// third commit
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+int getvalue(int n) {
+	if (n <= 3) {
+		return dp[n];
+	}
+	else {
+		if (dp[n] == 0) {
+			dp[n] = getvalue(n-1)+ getvalue(n - 2) + getvalue(n - 3);
+			return dp[n];
+		}
+		else {
+			return dp[n];
+		}
+	}
+}
